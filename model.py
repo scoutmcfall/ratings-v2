e99240e42559050479e064c05b843056d4cc3539
Scout McFall <scoutmknz@gmail.com>
2021-09-29 19:58:22 +0000

    add classes

--- model.py
+++ model.py
@@ -5,9 +5,62 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Replace this with your code!
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+class Movie(db.Model):
+    """A movie."""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    title = db.Column(db.String, unique=True)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f'<Movie title = {self.title} \nmovie_id={self.movie_id}>'
+
+class Rating(db.Model):
+    """A rating."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer,
+                    autoincrement=True,
+                    primary_key=True)
+    score = db.Column(db.Integer)
+    # score = db.Column(db.Integer, unique=True)
+    movie_id = db.Column(db.Integer,
+                        db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.user_id"))
+    
+    movie = db.relationship("Movie", backref="ratings")
+    user = db.relationship("User", backref="ratings") 
 
 
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} \nscore = {self.score}>'
+        
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
